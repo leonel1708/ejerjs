2b57b4b56b0e70de9392928507126a9bbe866cc9
Armando Alejandro <aalejandro1708@gmail.com>
2016-03-20 15:22:04 -0500

    Script con errores para practica en clase

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-console.log("Inicio de Programa");
-alert("hola Mundo");
-document.write("<h1>Hola Java Script</h1>");
-console.log("Final del Programa");
+console.log(Inicio de Programa");
+alert(hola Mundo);
+document.write("<h1>Hola Java Script</h1>")
+console.log("Final del Programa);
