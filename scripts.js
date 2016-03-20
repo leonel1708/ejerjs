91ef6f8926b2712f725958b88a71f254a6f94173
Armando Alejandro <aalejandro1708@gmail.com>
2016-03-20 17:17:41 -0500

    interaccion con js cuadros de preguntas y frases imprimir en documento

--- scripts.js
+++ scripts.js
@@ -1,4 +1,27 @@
-console.log("Inicio de Programa");
-alert("hola Mundo");
-document.write("<h1>Hola Java Script</h1>");
-console.log("Final del Programa");
+/*var equipo = prompt("Cual es tu equipo favorito?");
+console.log("El equipo es " + equipo);
+document.write("<h1>El equipo es " + equipo +"</h1>")*/
+
+/*var clave = prompt("Hola Usuario, ingrese su clave");
+console.log("La longitud de la clave es: "+clave.length);*/
+
+
+/*var clave = prompt("Hola Usuario, ingrese su clave en minusculas!");
+console.log("La longitud de la clave es: "+clave.length);
+console.log("TA-DA!!!. Su clave es: "+clave.toUpperCase());
+console.log("El valor original de la clave es: "+clave);*/
+
+alert("BIENVENIDOS AL CUENTA CUENTOS :)");
+var sustantivo = prompt("Inserte un sustantivo!");
+var adjetivo = prompt("Ahora ingresa un adjetivo!");
+var verbo = prompt("Por último ingresa un verbo!");
+document.write("<h1>EL CUENTA CUENTOS</h1>");
+document.write("<p>EL " + adjetivo + " " + sustantivo + " solía " + verbo +" los domingos en la tarde.</p>");
+document.write("<p>Un sabado en la mañana, el " + adjetivo + " " + sustantivo + " deseaba " + verbo +" junto con su familia.</p>");
+document.write("<p>Cada vez que el " + adjetivo + sustantivo + " " + verbo +" todos se divertían.</p>");
+
+/*console.log("La longitud de la clave es: "+clave.length);
+console.log("TA-DA!!!. Su clave es: "+clave.toUpperCase());
+console.log("El valor original de la clave es: "+clave);*/
+
+
